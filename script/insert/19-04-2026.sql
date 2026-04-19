e654c032c6942f9be98fc0dac6f04592f3386c1a
AlphaTech23 <myahaintso@gmail.com>
2026-04-19 22:46:23 +0300

    Insertion de donnees

---
+++ script/insert/19-04-2026.sql
@@ -0,0 +1,151 @@
+INSERT INTO situation_familiale (libelle) VALUES ('Célibataire');
+INSERT INTO situation_familiale (libelle) VALUES ('Marié(e)');
+INSERT INTO situation_familiale (libelle) VALUES ('Divorcé(e)');
+INSERT INTO situation_familiale (libelle) VALUES ('Veuf/Veuve');
+
+INSERT INTO nationalite (libelle) VALUES('Afghane');
+INSERT INTO nationalite (libelle) VALUES('Algérienne');
+INSERT INTO nationalite (libelle) VALUES('Allemande');
+INSERT INTO nationalite (libelle) VALUES('Américaine');
+INSERT INTO nationalite (libelle) VALUES('Andorrane');
+INSERT INTO nationalite (libelle) VALUES('Angolaise');
+INSERT INTO nationalite (libelle) VALUES('Argentine');
+INSERT INTO nationalite (libelle) VALUES('Arménienne');
+INSERT INTO nationalite (libelle) VALUES('Australienne');
+INSERT INTO nationalite (libelle) VALUES('Autrichienne');
+INSERT INTO nationalite (libelle) VALUES('Belge');
+INSERT INTO nationalite (libelle) VALUES('Bolivienne');
+INSERT INTO nationalite (libelle) VALUES('Brésilienne');
+INSERT INTO nationalite (libelle) VALUES('Bulgare');
+INSERT INTO nationalite (libelle) VALUES('Burkinabé');
+INSERT INTO nationalite (libelle) VALUES('Cambodgienne');
+INSERT INTO nationalite (libelle) VALUES('Camerounaise');
+INSERT INTO nationalite (libelle) VALUES('Canadienne');
+INSERT INTO nationalite (libelle) VALUES('Chilienne');
+INSERT INTO nationalite (libelle) VALUES('Chinoise');
+INSERT INTO nationalite (libelle) VALUES('Colombienne');
+INSERT INTO nationalite (libelle) VALUES('Comorienne');
+INSERT INTO nationalite (libelle) VALUES('Congolaise');
+INSERT INTO nationalite (libelle) VALUES('Coréenne');
+INSERT INTO nationalite (libelle) VALUES('Costaricienne');
+INSERT INTO nationalite (libelle) VALUES('Croate');
+INSERT INTO nationalite (libelle) VALUES('Danoise');
+INSERT INTO nationalite (libelle) VALUES('Egyptienne');
+INSERT INTO nationalite (libelle) VALUES('Emiratie');
+INSERT INTO nationalite (libelle) VALUES('Espagnole');
+INSERT INTO nationalite (libelle) VALUES('Estonienne');
+INSERT INTO nationalite (libelle) VALUES('Finlandaise');
+INSERT INTO nationalite (libelle) VALUES('Française');
+INSERT INTO nationalite (libelle) VALUES('Gabonaise');
+INSERT INTO nationalite (libelle) VALUES('Géorgienne');
+INSERT INTO nationalite (libelle) VALUES('Ghanéenne');
+INSERT INTO nationalite (libelle) VALUES('Grecque');
+INSERT INTO nationalite (libelle) VALUES('Guinéenne');
+INSERT INTO nationalite (libelle) VALUES('Haïtienne');
+INSERT INTO nationalite (libelle) VALUES('Hondurienne');
+INSERT INTO nationalite (libelle) VALUES('Hongroise');
+INSERT INTO nationalite (libelle) VALUES('Indienne');
+INSERT INTO nationalite (libelle) VALUES('Indonésienne');
+INSERT INTO nationalite (libelle) VALUES('Iranienne');
+INSERT INTO nationalite (libelle) VALUES('Irlandaise');
+INSERT INTO nationalite (libelle) VALUES('Islandaise');
+INSERT INTO nationalite (libelle) VALUES('Italienne');
+INSERT INTO nationalite (libelle) VALUES('Jamaïcaine');
+INSERT INTO nationalite (libelle) VALUES('Japonaise');
+INSERT INTO nationalite (libelle) VALUES('Kazakhe');
+INSERT INTO nationalite (libelle) VALUES('Kényane');
+INSERT INTO nationalite (libelle) VALUES('Kirghize');
+INSERT INTO nationalite (libelle) VALUES('Koweitienne');
+INSERT INTO nationalite (libelle) VALUES('Laotienne');
+INSERT INTO nationalite (libelle) VALUES('Lettone');
+INSERT INTO nationalite (libelle) VALUES('Libanaise');
+INSERT INTO nationalite (libelle) VALUES('Libérienne');
+INSERT INTO nationalite (libelle) VALUES('Lituanienne');
+INSERT INTO nationalite (libelle) VALUES('Luxembourgeoise');
+INSERT INTO nationalite (libelle) VALUES('Malagasy');
+INSERT INTO nationalite (libelle) VALUES('Malaisienne');
+INSERT INTO nationalite (libelle) VALUES('Malienne');
+INSERT INTO nationalite (libelle) VALUES('Marocaine');
+INSERT INTO nationalite (libelle) VALUES('Mauricienne');
+INSERT INTO nationalite (libelle) VALUES('Mauritanienne');
+INSERT INTO nationalite (libelle) VALUES('Mexicaine');
+INSERT INTO nationalite (libelle) VALUES('Moldave');
+INSERT INTO nationalite (libelle) VALUES('Monégasque');
+INSERT INTO nationalite (libelle) VALUES('Mongole');
+INSERT INTO nationalite (libelle) VALUES('Namibienne');
+INSERT INTO nationalite (libelle) VALUES('Népalaise');
+INSERT INTO nationalite (libelle) VALUES('Nicaraguayenne');
+INSERT INTO nationalite (libelle) VALUES('Nigériane');
+INSERT INTO nationalite (libelle) VALUES('Norvégienne');
+INSERT INTO nationalite (libelle) VALUES('Néo-Zélandaise');
+INSERT INTO nationalite (libelle) VALUES('Omanaise');
+INSERT INTO nationalite (libelle) VALUES('Ougandaise');
+INSERT INTO nationalite (libelle) VALUES('Ouzbèke');
+INSERT INTO nationalite (libelle) VALUES('Pakistanaise');
+INSERT INTO nationalite (libelle) VALUES('Panaméenne');
+INSERT INTO nationalite (libelle) VALUES('Paraguayenne');
+INSERT INTO nationalite (libelle) VALUES('Péruvienne');
+INSERT INTO nationalite (libelle) VALUES('Philippine');
+INSERT INTO nationalite (libelle) VALUES('Polonaise');
+INSERT INTO nationalite (libelle) VALUES('Portugaise');
+INSERT INTO nationalite (libelle) VALUES('Qatarienne');
+INSERT INTO nationalite (libelle) VALUES('Roumaine');
+INSERT INTO nationalite (libelle) VALUES('Russe');
+INSERT INTO nationalite (libelle) VALUES('Sénégalaise');
+INSERT INTO nationalite (libelle) VALUES('Serbe');
+INSERT INTO nationalite (libelle) VALUES('Singapourienne');
+INSERT INTO nationalite (libelle) VALUES('Slovaque');
+INSERT INTO nationalite (libelle) VALUES('Slovène');
+INSERT INTO nationalite (libelle) VALUES('Soudanaise');
+INSERT INTO nationalite (libelle) VALUES('Sri Lankaise');
+INSERT INTO nationalite (libelle) VALUES('Sud-Africaine');
+INSERT INTO nationalite (libelle) VALUES('Suédoise');
+INSERT INTO nationalite (libelle) VALUES('Suisse');
+INSERT INTO nationalite (libelle) VALUES('Tanzanienne');
+INSERT INTO nationalite (libelle) VALUES('Tchadienne');
+INSERT INTO nationalite (libelle) VALUES('Tchèque');
+INSERT INTO nationalite (libelle) VALUES('Thailandaise');
+INSERT INTO nationalite (libelle) VALUES('Tunisienne');
+INSERT INTO nationalite (libelle) VALUES('Turque');
+INSERT INTO nationalite (libelle) VALUES('Ukrainienne');
+INSERT INTO nationalite (libelle) VALUES('Uruguayenne');
+INSERT INTO nationalite (libelle) VALUES('Vénézuélienne');
+INSERT INTO nationalite (libelle) VALUES('Vietnamienne');
+INSERT INTO nationalite (libelle) VALUES('Zambienne');
+INSERT INTO nationalite (libelle) VALUES('Zimbabwéenne');
+
+INSERT INTO type_demande (libelle) VALUES ('Nouveau titre');
+INSERT INTO type_demande (libelle) VALUES ('Duplicata');
+INSERT INTO type_demande (libelle) VALUES ('Transfert');
+
+INSERT INTO categorie_demande (libelle) VALUES ('Travailleur');
+INSERT INTO categorie_demande (libelle) VALUES ('Investisseur');
+
+INSERT INTO document (libelle, type) VALUES ('02 photos d''identité récentes', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Notice de renseignement', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Demande adressée au Ministère de l''Intérieur (email + téléphone)', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Copie certifiée visa en cours', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Copie certifiée 1ère page passeport', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Copie carte résident (si applicable)', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Certificat de résidence Madagascar', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Extrait casier judiciaire (-3mois)', 'COMMUN');
+INSERT INTO document (libelle, type) VALUES ('Statut de la Société (obligatoire)', 'SPECIFIQUE');
+INSERT INTO document (libelle, type) VALUES ('Extrait RCS (Registre de commerce)', 'SPECIFIQUE');
+INSERT INTO document (libelle, type) VALUES ('Carte fiscale à jour', 'SPECIFIQUE');
+INSERT INTO document (libelle, type) VALUES ('Autorisation emploi (Ministère Fonction Publique)', 'SPECIFIQUE');
+INSERT INTO document (libelle, type) VALUES ('Attestation d''emploi originale (employeur)', 'SPECIFIQUE');
+
+INSERT INTO categorie_document (id_categorie_demande, id_document)
+SELECT id, 9 FROM categorie_demande WHERE libelle = 'Investisseur';
+
+INSERT INTO categorie_document (id_categorie_demande, id_document)
+SELECT id, 10 FROM categorie_demande WHERE libelle = 'Investisseur';
+
+INSERT INTO categorie_document (id_categorie_demande, id_document)
+SELECT id, 11 FROM categorie_demande WHERE libelle = 'Investisseur';
+
+INSERT INTO categorie_document (id_categorie_demande, id_document)
+SELECT id, 12 FROM categorie_demande WHERE libelle = 'Travailleur';
+
+INSERT INTO categorie_document (id_categorie_demande, id_document)
+SELECT id, 13 FROM categorie_demande WHERE libelle = 'Travailleur';
