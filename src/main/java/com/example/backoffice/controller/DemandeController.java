b4b1cf5abf1b66494e0d92b91d051da2bd6d8d27
AlphaTech23 <myahaintso@gmail.com>
2026-04-21 21:48:35 +0300

    Update DemandeController.java

--- src/main/java/com/example/backoffice/controller/DemandeController.java
+++ src/main/java/com/example/backoffice/controller/DemandeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.backoffice.entity.CategorieDemande;
-import com.example.backoffice.entity.Demande;
 import com.example.backoffice.entity.Demandeur;
 import com.example.backoffice.entity.Document;
 import com.example.backoffice.entity.Nationalite;
