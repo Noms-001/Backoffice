eab49d0f8d3680b98aae972ade1485386f8cf336
AlphaTech23 <myahaintso@gmail.com>
2026-04-26 21:35:12 +0300

    Update repository

--- src/main/java/com/example/backoffice/repository/DemandeurRepository.java
+++ src/main/java/com/example/backoffice/repository/DemandeurRepository.java
@@ -1,8 +1,11 @@
 package com.example.backoffice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backoffice.entity.Demandeur;
 
 public interface DemandeurRepository extends JpaRepository<Demandeur, Long> {
-    
+
+    List<Demandeur> findByNomAndPrenomAndAdresseMada(String nom, String prenom, String adresseMada);
 }
--- src/main/java/com/example/backoffice/repository/PasseportRepository.java
+++ src/main/java/com/example/backoffice/repository/PasseportRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.backoffice.entity.Passeport;
 
 public interface PasseportRepository extends JpaRepository<Passeport, Long> {
-    
+    Passeport findByReference(String reference);
 }
