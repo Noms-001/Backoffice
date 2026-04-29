5ff0fc91e5a24e21adac96b9e3b3691519f37595
AlphaTech23 <myahaintso@gmail.com>
2026-04-29 13:35:11 +0300

    Creation des modeles CarteResident et Visa

---
+++ src/main/java/com/example/backoffice/entity/Visa.java
@@ -0,0 +1,36 @@
+package com.example.backoffice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "visa")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Visa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String reference;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_passeport", nullable = false)
+    private Passeport passeport;
+
+    @Column(name = "date_debut", nullable = false)
+    private LocalDate dateDebut;
+
+    @Column(name = "date_fin", nullable = false)
+    private LocalDate dateFin;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_demande", nullable = false)
+    private Demande demande;
+}
