a71d63438ecbf1162db62b7801cb69a20dfe7b14
Noms-0001 <myahaintso@gmail.com>
2026-05-10 22:12:18 +0300

    Creation de nouvelles classes utilitaires

--- src/main/java/com/example/backoffice/util/QRCodeGenerator.java
+++ src/main/java/com/example/backoffice/util/QRCodeGenerator.java
@@ -1,24 +1,29 @@
 package com.example.backoffice.util;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.WriterException;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
 public class QRCodeGenerator {
+
     public static byte[] generateQRCode(String text, int width, int height) {
         try {
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
             BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
-            return pngOutputStream.toByteArray();
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+            return outputStream.toByteArray();
+
         } catch (WriterException | IOException e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("Erreur lors de la génération du QR Code", e);
         }
     }
 }
